d9969c6935228b08359b2af97fa9808b824e17f9
Pekka Korolainen <pekka.korolainen@gmail.com>
2020-07-28 17:27:19 +0300

    a 500x500 window that closes when you hit X

--- gol.cpp
+++ gol.cpp
@@ -1,6 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[]) 
 {
 	sf::RenderWindow window(sf::VideoMode(500,500), "Game of Life");
+
+	while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
 }
